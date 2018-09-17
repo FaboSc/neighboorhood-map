8bfab14777cd5284ffb56a98eca9664a34bac60d
Fabian Schmerbeck <fabian.schmerbeck@pentasys.de>
2018-09-17 09:25:03 +0200

    added functionality

--- javascript/app.js
+++ javascript/app.js
@@ -4,99 +4,166 @@ var viewModel;
 
 function DataModel() {
     var self = this;
+    var infoWindow = new google.maps.InfoWindow();
 
-    map = new google.maps.Map(document.getElementById('map'), {
+    // Set inital map location
+    self.map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 48.5440307, lng: 12.131067 } ,
         zoom: 14,
         disableDefaultUI: true
     });
 
-    this.wikiData =
+    // Get wikipedia data, if successfull create info windows, if not inform
+    // the user
+    $.ajax({
+        url: 'http://en.wikipedia.org/w/api.php',
+        data: {
+            action: 'query',
+            titles: "Englischer Garten|Trausnitz Castle|Landshut Wedding|St. " +
+                "Martin's Church, Landshut|Munich Airport",
+            prop: "extracts",
+            exintro: true,
+            format: 'json'
+        },
+        dataType: 'jsonp',
+    }).done(function(data) {
+        wikiData = data.query.pages;
 
+        // Create info-windows with wikipedia introduction
+        self.locationMarkers.forEach(function (marker) {
+            marker.infowindow = new google.maps.InfoWindow ({
+                content: wikiData[marker.wiki_id].extract
+            });
+
+            marker.setMap(self.map);
+            marker.content = wikiData[marker.wiki_id].extract;
+
+            marker.addListener('click', function() {
+               self.handleMarkerClick(this);
+            });
+        });
+    }).fail(function() {
+        alert("Cannot reach wikipedia, info-windows cannot be filled. " +
+            "Please try again later.");
+    });
+
+    // Create location markers
     this.locationMarkers = [new google.maps.Marker({
         id: 1,
         wiki_id: 3772076,
         position: { lat: 48.5318984, lng: 12.1458682 },
-        title: 'Burg Trausnitz, Landshut'
+        title: 'Burg Trausnitz, Landshut',
+        content: ""
     }),
         new google.maps.Marker({
             id: 2,
-            position: { lat: 48.5347372, lng: 12.1491855 },
-            title: 'St. Jodok, Landshut'
+            wiki_id:266998,
+            position: { lat: 48.3540837, lng: 11.7965729 },
+            title: 'Munich Airport',
+            content: ""
         }),
         new google.maps.Marker({
             id: 3,
+            wiki_id:403255,
             position: { lat: 48.1642359, lng: 11.6033635 },
-            title: 'English Garden, Munich'
+            title: 'English Garden, Munich',
+            content: ""
         }),
         new google.maps.Marker({
             id: 4,
+            wiki_id:3730085,
             position: { lat: 48.5341438, lng: 12.1512669 },
-            title: 'Sankt Martin Church, Landshut'
+            title: 'Sankt Martin Church, Landshut',
+            content: ""
         }),
         new google.maps.Marker({
             id: 5,
-            position: { lat: 48.1400823, lng: 11.5709193 },
-            title: 'Micheal Jackson Memorial, Munich'
+            wiki_id:3771816,
+            position: { lat: 48.5329512, lng: 12.1468782 },
+            title: 'Landshut Wedding',
+            content: ""
         })];
 
-    self.locationMarkers.forEach(function (marker) {
+    this.openInfoWindow = function(marker) {
+        if (infoWindow.marker != marker) {
+            infoWindow.setContent(marker.content);
+            infoWindow.marker = marker;
 
-        var infowindow = new google.maps.InfoWindow ({
-            content: "hello"
-        });
+            infoWindow.addListener('closeclick', function() {
+                infoWindow.marker = null;
+            });
 
-        marker.setMap(map);
-        marker.addListener('click', function() {
-            infowindow.open(map, marker)
-        });
-    });
-
-    //https://en.wikipedia.org/w/api.php?action=query&pageids=3772076&prop=extracts&format=json
-
-    $.ajax({
-        url: 'http://en.wikipedia.org/w/api.php',
-        data: {
-            action:'query',
-            pageids: 3772076,
-            prop:"extracts",
-            exintro: true,
-            format:'json'
-        },
-        dataType:'jsonp',
-        success: function(data) {
-            console.log(data.query.pages[3772076].extract);
-
+            infoWindow.open(self.map, marker);
         }
-    });
+    };
 
+    // Handle Click on Marker
+    this.handleMarkerClick = function(marker) {
 
+        // Open Info Window
+        self.openInfoWindow(marker);
 
+        // Start Bounce Animation
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function () { marker.setAnimation(null); },
+            750);
+    };
 
+    this.hideLocMarkers = function() {
+        self.locationMarkers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+    };
 
+    this.showLocMarkers = function(currentLocations) {
+        self.hideLocMarkers();
+
+        currentLocations.forEach(function(marker) {
+            marker.setMap(self.map);
+        });
+    };
 }
-
 
 
 function ViewModel() {
     var self = this;
 
-    this.filteredLocations = ko.observableArray([]);
+    self.filteredLocations = ko.observableArray([]);
+    self.filterString = ko.observable();
+    self.navVisible = ko.observable(true);
 
-    this.navVisible = ko.observable(true);
+    // Get all Locations
+    dataModel.locationMarkers.forEach(function(marker) {
+        self.filteredLocations.push(marker);
+    });
 
-    this.filteredLocations = dataModel.locationMarkers;
+    // Filter locations, based on the filterString
+    this.filterString.subscribe(function() {
+        self.filteredLocations.removeAll();
+        dataModel.locationMarkers.forEach(function (marker) {
+            var filterString = self.filterString().toLowerCase();
+            var title = marker.title.toLowerCase();
 
-    this.toggleNav = function() {
+            if (title.includes(filterString)) {
+                self.filteredLocations.push(marker);
+            }
+
+        });
+        dataModel.showLocMarkers(self.filteredLocations());
+
+    });
+
+    // Toggle navigation
+    self.toggleNav = function() {
         this.navVisible(!this.navVisible());
-        console.log(this.navVisible())
-    }
+    };
+
+    // Handle Click on Menu Item
+    self.handleMenuItemClick = function(data) {
+      dataModel.handleMarkerClick(dataModel.locationMarkers[data.id - 1]);
+    };
 
 }
-
-
-
-
 
 initMap = function () {
     dataModel = new DataModel();
@@ -104,3 +171,7 @@ initMap = function () {
     ko.applyBindings(viewModel);
 
 };
+
+gerror = function() {
+    alert("Could not reach google maps api, please try again later.");
+}
